5fc304ad17d0769378c85910149b6445337cd723
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-05-20 16:09:14 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-amber-400'>Duka</h1>
+      
+    </>
+  )
+}
+
+export default App
